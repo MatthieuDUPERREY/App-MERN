0a0d79f6978a9e580e31dd1b6028bae6a2e302c6
Matthieu Duperrey <matth.duperrey@gmail.com>
2023-12-12 09:58:12 -0500

    .env fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
     console.log("Mongo connecté");
   } catch (error) {
     console.error("Erreur de connexion à la base de données:", error.message);
-    process.exit(1); // Arrête le processus en cas d'erreur de connexion
+    process.exit();
   }
 };
 
