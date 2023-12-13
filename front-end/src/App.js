aeb0fb28bade03ea51239b2009fb985457a2a0e1
Matthieu Duperrey <matth.duperrey@gmail.com>
2023-12-13 10:59:56 -0500

    app.js reorganize

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [userId, setUserId] = useState("");
+
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="app_container">
+      <div className="login">
+        <h3>Hello</h3>
+        <input
+          type="text"
+          placeholder="Pseudo"
+          onChange={(e) => setUserId(e.target.value)}
+        />
+      </div>
     </div>
   );
 };
