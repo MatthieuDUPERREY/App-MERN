28539ecb8f2d7d3fab099c9e35c5a7551f700dda
Matthieu Duperrey <matth.duperrey@gmail.com>
2023-12-13 12:00:00 -0500

    add files NewPosts and Thread

--- front-end/src/components/NewPost.js
+++ front-end/src/components/NewPost.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
-const NewPost = () => {
+const NewPost = ({ userId }) => {
+  const [message, setMessage] = useState("");
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:5050/post", {
+      message,
+      author: userId,
+    });
+  };
+
   return (
-    <div>
-      <h1>New Post</h1>
-    </div>
+    <form className="new-post-container" onSubmit={(e) => handleForm(e)}>
+      <textarea
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Hello world"
+      ></textarea>
+      <input type="submit" value="Envoyez le formulaire" />
+    </form>
   );
 };
 
