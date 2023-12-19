c1947cc624c0e1ea5a8a59a026270f47809ee1c8
Matthieu Duperrey <matth.duperrey@gmail.com>
2023-12-19 14:19:34 -0500

    like and dislike button added

--- front-end/src/components/Post.js
+++ front-end/src/components/Post.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import LikePost from "./LikePost";
 const Post = ({ post, userId }) => {
   const dateFormater = (date) => {
     return new Date(date).toLocaleDateString("fr-Fr", {
@@ -19,6 +19,9 @@ const Post = ({ post, userId }) => {
           <p>posté le {dateFormater(post.createdAt)}</p>
         </div>
         <p>{post.message}</p>
+        <div className="icons-part">
+          <LikePost post={post} userId={userId} />
+        </div>
       </div>
     </div>
   );
